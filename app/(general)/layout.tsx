adb8bb95a95a79deeb204a0b8ce2cca570398a6d
richard-reyes <richardreyesarocutipalope@gmail.com>
2023-12-17 01:13:58 -0500

    navigation Next/Link

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,10 +1,14 @@
+import { NavBar } from "@/components";
 import { ReactNode } from "react";
 
 export default function GeneralLayout({ children }: { children: ReactNode }) {
     return (
-        <main className="flex flex-col items-center p-24">
-            <h1 className="text-5xl">Hola mundo </h1>
-            {children}
-        </main>
+        <>
+            <NavBar />
+            <main className="flex flex-col items-center p-24">
+                <h1 className="text-5xl">Hola mundo </h1>
+                {children}
+            </main>
+        </>
     );
 }
