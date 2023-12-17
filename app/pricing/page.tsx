224429ef7cac9063d0741ebdb4c65e3d0b4b6678
richard-reyes <richardreyesarocutipalope@gmail.com>
2023-12-16 23:30:10 -0500

    metadata - metatags

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'SEO pricing',
+ description: 'SEO pricing description',
+};
+
 export default function PricingPage() {
     return (
         <>
